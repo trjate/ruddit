ace1000e132fb4791503646bb1d4779f1717ae83
Trevor Terris <tjterris@gmail.com>
2015-06-12 15:08:46 -0400

    wrote route files.

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,32 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+root to: 'links#index'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+#User routes 
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+get '/users/new' to: 'users#new' # shows the new user sign-up form
+post '/users' to: 'users#create' # creates a user
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+#Links routes
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+get '/links/new' to: 'links#new' # show the form to create a new link 
+post '/links' to: 'links#create' # creates a link
+get '/link/:id' to: 'links#transfer' # shows me an individual link
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+#Comments routes
+
+get '/link/:id/comments' to: 'comments#new' # get the page for a link, its comments, and comment form 
+post '/link/:id/comments' to: 'comments#create'  # the form action for new comment, creates a comment (if logged in)
+
+
+#Session routes
+
+get '/users/login' to: 'users#login'
+post '/users/session' to: 'sessions#create'
+delete '/users/session' to: 'sessions#destroy'
+
+
 end
