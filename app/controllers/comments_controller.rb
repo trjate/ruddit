6d34c78051df5128b33a343f812c1c77435e2c8f
DM <DungeonMaster@DMs-MacBook-Air.local>
2015-06-14 20:11:08 -0400

    Got the URL titles to work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,21 @@
 class CommentsController < ApplicationController
 
-  def new
-    @action = comments_new_path
-    render :new
-  end
+
+
+def show
+  @link = Link.find(params[:id])
+
+  render :show
+end
+
+
 
   def create
 
     @comment = Comment.create(user_id: params[:user_id],
                         link_id: params[:link_id],
                         content: params[:content])
-                        
+
     redirect_to comments_new_path
   end
 
